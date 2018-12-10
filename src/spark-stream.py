8392080c93d22cbee6eefa1873befd2bb83d29f2
Abhishek Kumar Srivastava <asrivas3@ncsu.edu>
2018-12-10 16:24:26 -0500

    Modified Stream Code

--- src/spark-stream.py
+++ src/spark-stream.py
@@ -6,8 +6,12 @@ import json
 import os
 
 os.system("/opt/kafka/kafka_2.12-1.0.1/bin/kafka-topics.sh --zookeeper \"152.46.18.86:2181,152.46.18.71:2181,152.46.17.184:2181\" --delete --topic \"output\" > /dev/null 2>&1")
+os.system("/opt/kafka/kafka_2.12-1.0.1/bin/kafka-topics.sh --zookeeper \"152.46.18.86:2181,152.46.18.71:2181,152.46.17.184:2181\" --delete --topic \"words\" > /dev/null 2>&1")
 
 producer = KafkaProducer(bootstrap_servers='152.46.18.86:9092,152.46.18.71:9092,152.46.17.184:9092')
+producer.send('output',key = b'File Name',value = b'Wrong Words')
+producer.send('words',key = b'start',value = b'start')
+
 d = open('d1.txt')
 dic = list()
 
